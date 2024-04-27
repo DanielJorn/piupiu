1813f3d946c1c401e7343220da4ad252b040ee8d
anton <0954264@gmail.com>
2024-04-27 22:50:29 +0300

    add timer/reorder sprites

--- player.gd
+++ player.gd
@@ -1,50 +1,50 @@
 extends CharacterBody2D
 
-# attaching script to Node allows us to read Node's children using $<child/deeper_child>
-# onready is must have for some reason idk
 @onready var body = $body
 @onready var gun = $gun
 @onready var ship = $ship
 @onready var gun_tip = $gun/gun_tip
+@onready var shoot_timer = $shoot_timer
 
-# export експортує в едітор змінні які ти можеш через ползунки мінять
 @export var speed = 300
 @export var rotation_speed = 1.5
 @export var cannonball : PackedScene
+@export var fire_rate = 0.5
 
 var rotation_direction = 0
 var gunRotationDegrees = 0
+var can_shoot = true
 
-# функція CanvasItem яка малює. Ctrl + click щоб побачити доку
 func _draw():
-	# локальна позиція - відносно рутової ноди, глобальна - глобально лол
-	# перевод в локальну з глобальної треба, бо повертаючи пушку, локально кординати дула залишаються такі ж
 	draw_line(to_local(gun_tip.global_position), get_local_mouse_position(), Color.BLACK, 5)
 
 func get_input():
 	gunRotationDegrees = rad_to_deg(to_local(gun.global_position).angle_to_point(get_local_mouse_position()))
 	rotation_direction = Input.get_axis("left", "right") * (-1 if Input.get_axis("down", "up") == -1 else 1)
 	velocity = velocity*0.95 + (transform.x * (0.333+0.666*Input.get_axis("down", "up")) * speed)*0.05
-	#velocity = velocity*0.95 + (transform.x * speed)*0.05 + Input.get_axis("down", "up")
+
 	rotation_speed = rotation_speed*0.75 + (velocity.length()/speed)*2
-	if Input.is_action_just_pressed("shoot"):
+	if Input.is_action_just_pressed("shoot") and can_shoot:
 		shoot()
+		shoot_timer.wait_time = fire_rate
+		shoot_timer.start()
+		shoot_timer.timeout.connect(set_can_shoot)
+		can_shoot = false
+		print("shooot")
+		
+func set_can_shoot():
+	can_shoot = true
+	print("can shot true")
 
 func _physics_process(delta):
 	get_input()
 	rotation += rotation_direction * rotation_speed * delta
-
-	move_and_slide()
 	gun.rotation_degrees = gunRotationDegrees
+	
+	move_and_slide()
 	queue_redraw()
 
 func shoot():
 	var b = cannonball.instantiate()
 	owner.add_child(b)
 	b.transform = $gun/gun_tip.global_transform
-
-func _ready():
-	pass
-
-func _process(delta):
-	pass
