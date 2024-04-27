e10d59795a3fee4eeac940ba53bafd4d8b7c8a49
anton <0954264@gmail.com>
2024-04-27 23:00:29 +0300

    improve bullet

--- cannonball.gd
+++ cannonball.gd
@@ -1,6 +1,8 @@
 extends Area2D
 
 var speed = 1000
+@onready var visibility_notifier = $VisibilityNotifier
+@onready var death_timer = $DeathTimer
 
 func _physics_process(delta):
 	position += transform.x * speed * delta
@@ -9,3 +11,10 @@ func _on_body_entered(body):
 	if not body.is_in_group("player"):
 		body.queue_free()
 		queue_free()
+
+func _on_visibility_notifier_screen_exited():
+	death_timer.start(1)
+
+func _on_death_timer_timeout():
+	print("deleting bullet")
+	queue_free()
