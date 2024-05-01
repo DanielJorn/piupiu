29447595ee3727bec9fe93339fcb1d84e704002d
vlad <cyberlord2003@gmail.com>
2024-05-01 13:25:37 +0300

    movement code tweaked and linted

--- player.gd
+++ player.gd
@@ -8,7 +8,7 @@ extends CharacterBody2D
 #@onready var colider = $colider as CollisionPolygon2D
 
 @export var speed = 100
-@export var rotation_speed = 1.5
+@export var rotation_speed = 0
 @export var cannonball : PackedScene
 @export var fire_rate = 0.5
 
@@ -16,24 +16,21 @@ var rotation_direction = 0
 var gunRotationDegrees = 0
 var can_shoot = true
 
+
 func _draw():
 	draw_line(to_local(gun_tip.global_position), get_local_mouse_position(), Color.BLACK, 5)
+
 
 func _on_body_entered(body):
 	body.queue_free()
 	queue_free()
 
+
 func get_input():
 	gunRotationDegrees = rad_to_deg(
-		to_local(gun.global_position)
-		.angle_to_point(get_local_mouse_position())
+			to_local(gun.global_position)
+			.angle_to_point(get_local_mouse_position())
 	)
-	# if no Input, then assign 1, otherwise -1
-	rotation_direction = Input.get_axis("left", "right") * (-1 if Input.get_axis("down", "up") == -1 else 1)
-	
-	velocity = velocity*0.95 + (transform.x * (1 + 2*Input.get_axis("down", "up")) * speed)*0.05
-
-	rotation_speed = rotation_speed*0.75 + (velocity.length()/speed)*2
 	if Input.is_action_just_pressed("shoot") and can_shoot:
 		shoot()
 		shoot_timer.wait_time = fire_rate
@@ -41,10 +38,31 @@ func get_input():
 		shoot_timer.timeout.connect(set_can_shoot)
 		can_shoot = false
 		print("shooot")
-		
+	
+	var forwardDrive = Input.get_axis("down", "up")
+	# if w pressed, move with double speed; if no Input, then assign 1
+	if forwardDrive == 1:
+		forwardDrive = 2
+	elif forwardDrive == 0:
+		forwardDrive = 1
+	var inertia = 0.95
+	velocity = velocity * inertia + (transform.x * forwardDrive * speed) * (1 - inertia)
+	
+	var rotationDrive = Input.get_axis("left", "right")
+	var rotation_inertia = 0.5
+	var mov_impact = 1
+	var base_rotation_speed = 1
+	rotation_direction = rotationDrive * sign(forwardDrive)
+	rotation_speed =\
+		rotation_speed * rotation_inertia\
+		+ velocity.length() / speed * mov_impact\
+		+ base_rotation_speed
+
+
 func set_can_shoot():
 	can_shoot = true
 	print("can shot true")
+
 
 func _physics_process(delta):
 	get_input()
@@ -53,6 +71,7 @@ func _physics_process(delta):
 	
 	move_and_slide()
 	queue_redraw()
+
 
 func shoot():
 	var b = cannonball.instantiate()
